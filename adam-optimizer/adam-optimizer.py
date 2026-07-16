4b40a393f8b948c230c4e98c3d88bc47191ce50f
Deon Xavier Pereira <deonxavier2019@gmail.com>
2026-07-16 13:21:52 +0530

    Solve adam-optimizer

---
+++ adam-optimizer/adam-optimizer.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+def adam_step(param, grad, m, v, t, lr=1e-3, beta1=0.9, beta2=0.999, eps=1e-8):
+    """
+    One Adam optimizer update step.
+    Return (param_new, m_new, v_new).
+    """
+    
+    param = np.array(param)
+    grad = np.array(grad)
+    m = np.array(m)
+    v = np.array(v)
+    
+    m = beta1 * m + (1 - beta1) * grad 
+    v = beta2 * v + (1 - beta2) * np.power(grad, 2)
+    m_cap = m / (1 - beta1 ** t)
+    v_cap = v / (1 - beta2 ** t)
+    param = param - lr * (m_cap / (np.sqrt(v_cap) + eps))
+    return param, m, v
